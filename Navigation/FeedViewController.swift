efbb4f5c1e843d2febde7cc0b13414c70ad84eef
Roman Romanov <rromanov.vk@gmail.com>
2023-02-12 00:11:37 +0500

    Additional task

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -12,9 +12,48 @@ struct Post {
 }
 
 final class FeedViewController: UIViewController {
+    
+    lazy private var stackView: UIStackView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.axis = .vertical
+        $0.distribution = .fillEqually
+        $0.spacing = 10
+        
+        return $0
+    }(UIStackView())
+    
+    private let numberOneButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Button #1", for: .normal)
+        button.backgroundColor = .systemBlue
+        
+        button.layer.cornerRadius = 10
+        
+        button.layer.shadowOffset = .init(width: 7, height: 7)
+        button.layer.shadowRadius = 7
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.7
+
+        return button
+    }()
+    
+    private let numberTwoButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Button #2", for: .normal)
+        button.backgroundColor = .systemBlue
+        
+        button.layer.cornerRadius = 10
+        
+        button.layer.shadowOffset = .init(width: 7, height: 7)
+        button.layer.shadowRadius = 7
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.7
+
+        return button
+    }()
 
     private let postButton: UIButton = {
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
+        let button = UIButton()
         button.setTitle("Посмотри меня", for: .normal)
         button.setTitle("Узнай новость!", for: .highlighted)
         button.backgroundColor = .black
@@ -25,7 +64,25 @@ final class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .cyan
-        setupButton()
+//        setupButton()
+        layout()
+    }
+    
+    private func layout() {
+        view.addSubview(stackView)
+        stackView.addArrangedSubview(numberOneButton)
+        stackView.addArrangedSubview(numberTwoButton)
+        
+        numberOneButton.addTarget(self, action: #selector(tapAction), for: .touchUpInside)
+        numberTwoButton.addTarget(self, action: #selector(tapAction), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            stackView.heightAnchor.constraint(equalToConstant: 110),
+            stackView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: 0),
+            stackView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor, constant: 0),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
+        ])
     }
 
     private func setupButton() {
